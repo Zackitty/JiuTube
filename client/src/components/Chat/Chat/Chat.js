e1f12d68326fc7ec20a1c64aa750b2ab73d61cc2
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-10-29 15:05:05 -0500

    Add icon

--- client/src/components/Chat/Chat/Chat.js
+++ client/src/components/Chat/Chat/Chat.js
@@ -100,7 +100,7 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-          <BlockButton open={isOpen} onClose={() => setIsOpen(false)}/>
+          {/* <BlockButton open={isOpen} onClose={() => setIsOpen(false)}/> */}
           <InfoBar room={room} />
           <Messages messages={messages} name={name} />
           <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
