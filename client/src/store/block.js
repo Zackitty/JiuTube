5c30d532eed2ad6ab0a61e6f70423a0bd312ecdd
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-11-04 11:21:25 -0600

    add to block store

--- client/src/store/block.js
+++ client/src/store/block.js
@@ -5,6 +5,47 @@ const GET_BLOCKS = 'JIUTUBE/blocks/GET_BLOCKS';
 const ADD_BLOCK = 'JIUTUBE/blocks/ADD_BLOCK';
 const REMOVE_BLOCK = 'JIUTUBE/blocks/REMOVE_BLOCK';
 
+export const fetchBlocks = (USER_ID) => async dispatch => {
+  
+  const response = await fetch(`${apiUrl}/${USER_ID}`)
+  if (!response.ok) {
+    throw response;
+  }
+  const data = await response.json()
+  dispatch(getBlocks(data))
+}
+
+export const setBlock = (user_id, blocked_id) => async dispatch => {
+  try {
+    const formData = new FormData();
+    formData.append("user_id", user_id)
+    formData.append("blocked_id", blocked_id)
+    
+    // if (profPic !== "") {
+    //   formData.append("profPic", profPic, `${firstName}-profpic`)
+    // }
+
+    const response = await fetch(`${apiUrl}/users/signup`, {
+      method: 'post',
+      body: formData
+    });
+
+    if (!response.ok) {
+      throw response
+    }
+
+}
+
+  export const getBlocks= (data) => ({
+    type: GET_BLOCKS,
+    data
+  })
+
+  export const addBlock= (data) => ({
+    type: ADD_BLOCK,
+    data
+  })
+
 export default function reducer(state = {}, action) {
   Object.freeze(state)
   const newState = Object.assign({}, state);
@@ -13,9 +54,6 @@ export default function reducer(state = {}, action) {
       return action.data
     }
     case ADD_BLOCK: {
-      return action.data
-    }
-    case REMOVE_BLOCK: {
       return action.data
     }
     default: return newState
