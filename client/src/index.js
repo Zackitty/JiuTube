e490a53ccf009d88c04cb7d0dba9c7117952deb6
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-07-19 12:11:26 -0500

    da

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,7 @@ ReactDOM.render(
       <Grommet theme={theme}>
         <App />
       </Grommet>
-      </PersistGate>
+      </PersistGate>d
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
