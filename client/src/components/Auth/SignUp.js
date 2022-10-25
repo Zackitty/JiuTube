c58b02ab4f6a5dcf42733feaddb7414a30621f74
zackitty <theelfenliedpictureshow@yahoo.com>
2022-10-25 18:11:40 -0500

    fix issues

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -22,10 +22,10 @@ const SignUp = (props) => {
   const handleFileUpload = async (e) => {
     // Configures AWS Cloud Technology to allow users to store pictures
     const config = {
-      bucketName: 'jiutubeawsbucket',
+      bucketName: 'jiutjitsutube',
       region: 'us-east-2',
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
     }
     //  take the file  from the event of a user uploading a file and
     // uploads it to the AWS cloud bucket and sets the URL to the state
